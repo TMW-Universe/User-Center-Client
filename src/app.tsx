1d772cc79f14180f4b2c604ade82df2840dd72bb
Joel Campos <joelcamposoliva9000@gmail.com>
2023-08-27 21:34:52 +0200

    Fix: missing broser router

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import Router from "./router/router";
 import routes_definition from "./router/routes-definition";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <Router routes={routes_definition} />
-    </>
+    </BrowserRouter>
   );
 }
--- src/router/routes-definition.ts
+++ src/router/routes-definition.ts
@@ -1,5 +1,11 @@
 import { Route } from "./router";
+import { routes } from "./routes";
 
-const routes_definition: Route[] = [];
+const routes_definition: Route[] = [
+  {
+    path: routes.MAIN_PAGE(),
+    loader: () => import("../pages/main.page"),
+  },
+];
 
 export default routes_definition;
