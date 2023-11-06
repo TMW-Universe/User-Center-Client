3466fa7daab2fb0aaf42feb35a77e98dbc98c1de
Joel Campos <joelcamposoliva9000@gmail.com>
2023-11-06 21:53:00 +0100

    Move modules

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -6,6 +6,7 @@ import { Flex, Typography } from "antd";
 import UserProfileImage from "../user/profile/user-profile-image";
 import { useTranslation } from "react-i18next";
 import { Translations } from "../../i18n/translations.enum";
+import ProfileModules from "../user/modules/profile-modules";
 
 const { Title, Text } = Typography;
 
@@ -16,14 +17,17 @@ export default function Welcome() {
   if (!isAuthenticated) throw new Error();
 
   return (
-    <Flex vertical align="center" gap="1.5em">
-      <UserProfileImage user={account} />
-      <Flex vertical align="center">
-        <Title level={4}>
-          {t("user-info.Welcome", { name: getFullName(account) })}
-        </Title>
-        <Text>{t("user-info.Description")}</Text>
+    <Flex vertical gap={"2.5em"}>
+      <Flex vertical align="center" gap="1.5em">
+        <UserProfileImage user={account} />
+        <Flex vertical align="center">
+          <Title level={4}>
+            {t("user-info.Welcome", { name: getFullName(account) })}
+          </Title>
+          <Text>{t("user-info.Description")}</Text>
+        </Flex>
       </Flex>
+      <ProfileModules />
     </Flex>
   );
 }
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,12 +1,5 @@
-import { Flex } from "antd";
 import Welcome from "../components/welcome/welcome";
-import ProfileModules from "../components/user/modules/profile-modules";
 
 export default function HomePage() {
-  return (
-    <Flex vertical gap={"2.5em"}>
-      <Welcome />
-      <ProfileModules />
-    </Flex>
-  );
+  return <Welcome />;
 }
