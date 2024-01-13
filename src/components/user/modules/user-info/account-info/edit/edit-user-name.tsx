db991f79ec3aca8e477b830611fa6580b836feab
Joel Campos <joelcamposoliva9000@gmail.com>
2024-01-13 13:14:27 +0100

    Form UI

--- src/components/user/modules/user-info/account-info/edit/edit-user-name.tsx
+++ src/components/user/modules/user-info/account-info/edit/edit-user-name.tsx
@@ -2,8 +2,13 @@ import { useForm } from "react-handled-forms";
 import Form from "react-handled-forms/dist/components/form";
 import { InferType, object, string } from "yup";
 import TextFormItem from "../../../../../common/form/items/text/text.form-item";
-import { Button, Drawer, Flex } from "antd";
+import { Button, Drawer, Flex, Typography } from "antd";
 import { SaveOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
+import { Translations } from "../../../../../../i18n/translations.enum";
+import { useEffect } from "react";
+
+const { Text } = Typography;
 
 type Props = {
   open: boolean;
@@ -30,23 +35,70 @@ export default function EditUserName({
   open,
   onClose,
 }: Props) {
+  const { t } = useTranslation([Translations.USER_INFO, Translations.COMMON]);
+
   const form = useForm({
     objectSchema: FORM_SCHEMA,
     defaultValues: { name, firstSurname, secondSurname },
+    onSubmit: async (values) => {},
   });
+
+  useEffect(() => {
+    if (open) form.setFormState({ name, firstSurname, secondSurname });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [open]);
 
   return (
     <Drawer
       open={open}
       onClose={onClose}
-      title={""}
-      footer={<Button icon={<SaveOutlined />} block type="primary"></Button>}
+      title={t("edit.name.Title", { ns: Translations.USER_INFO })}
+      footer={
+        <Button
+          onClick={() => form.submit()}
+          icon={<SaveOutlined />}
+          block
+          type="primary"
+        >
+          {t("actions.Save", {
+            ns: Translations.COMMON,
+          })}
+        </Button>
+      }
     >
       <Form form={form}>
         <Flex gap={6} vertical>
-          <TextFormItem<FormType, "name"> name="name" />
-          <TextFormItem<FormType, "firstSurname"> name="firstSurname" />
-          <TextFormItem<FormType, "secondSurname"> name="secondSurname" />
+          <Flex vertical gap={3}>
+            <Text>
+              {t("edit.name.fields.name.Label", { ns: Translations.USER_INFO })}
+            </Text>
+            <TextFormItem<FormType, "name">
+              name="name"
+              componentProps={{ minLength: 2, maxLength: 32, showCount: true }}
+            />
+          </Flex>
+          <Flex vertical gap={3}>
+            <Text>
+              {t("edit.name.fields.firstSurname.Label", {
+                ns: Translations.USER_INFO,
+              })}
+            </Text>
+            <TextFormItem<FormType, "firstSurname">
+              name="firstSurname"
+              componentProps={{ minLength: 2, maxLength: 64, showCount: true }}
+            />
+          </Flex>
+          <Flex vertical gap={3}>
+            <Text>
+              {t("edit.name.fields.secondSurname.Label", {
+                ns: Translations.USER_INFO,
+              })}
+            </Text>
+            <TextFormItem<FormType, "secondSurname">
+              name="secondSurname"
+              componentProps={{ minLength: 2, maxLength: 64, showCount: true }}
+            />
+          </Flex>
         </Flex>
       </Form>
     </Drawer>
